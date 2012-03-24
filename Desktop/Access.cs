352a67f72dc2f279661ab77221e0e4f30950d06d
Kamil Pomykala <ja@kamilpomykala.com>
2012-03-24 14:27:37 +0100

    Struktura katalogow, schemat bazy

---
+++ Desktop/Access.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DziennikLekcyjny
+{
+    class Access
+    {
+        public Access() { }
+
+        public virtual Int32 ID { get; set; }
+        public virtual String NAME { get; set; }
+        public virtual String PASSWORD { get; set; }
+        public virtual Int32 PERSONID { get; set; }
+        public virtual Int32 ROLEID { get; set; }
+    }
+}
